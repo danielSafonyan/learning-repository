6e63d7534385848c6c1bd7167cab15c52336ff9a
danielSafonyan <danielsafonyan@gmail.com>
2022-08-30 14:30:36 +0100

    Appendix B exe-1 Done

--- ydkjs/ydkjs-bk-1/exe-appendix-b/practiving-comparisons.js
+++ ydkjs/ydkjs-bk-1/exe-appendix-b/practiving-comparisons.js
@@ -1,3 +1,7 @@
+// Let's practice working with value types and comparisons (Chapter 4, Pillar 3) where coercion will need to be involved.
+
+// scheduleMeeting(..) should take a start time (in 24-hour format as a string "hh:mm") and a meeting duration (number of minutes). It should return true if the meeting falls entirely within the work day (according to the times specified in dayStart and dayEnd); return false if the meeting violates the work day bounds.
+
 const dayStart = "07:30";
 const dayEnd = "17:45";
 
@@ -27,7 +31,6 @@ function scheduleMeeting(startTime, durationMinutes) {
         meetingEndHour++;
         meetingEndMinutes = meetingEndMinutes % 60;
     }
-    
 
     // create strings representing meetingStart and meetingEnd
     let meetingStart = `${meetingStartHour.toString().padStart(2, "0")}:${meetingStartMinutes.toString().padStart(2, "0")}`;
