cd0ff3e0f13dcb27da89ca6f64721ffcb825ce0f
danielSafonyan <danielsafonyan@gmail.com>
2022-08-31 21:24:24 +0100

    Finished Lifecycle of Variables

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function sayHi() {
-    console.log("Hi!");
+var studentName = "Kyle";
+
+{
+    console.log(studentName);
+    // ???
+
+    // ..
+
+    let studentName = "Suzy";
+
+    console.log(studentName);
+    // Suzy
 }
