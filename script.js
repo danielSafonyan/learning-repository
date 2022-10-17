fa919925997a77237f540dcc9ba59ff3b7527fcd
danielSafonyan <danielsafonyan@gmail.com>
2022-10-17 18:09:51 +0100

    Learning TDD

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-const img = document.querySelector('img');
-
-const newGifBtn = document.querySelector('#new-gif');
-newGifBtn.addEventListener('click', getGIF);
-
-function getGIF() {
-    const imgURL = 'http://api.giphy.com/v1/gifs/translate?api_key=rj975r52zrOqbpR5LBS4M68C4ZzRW5Xu&s=programming&weirdness=10'
-    let newImg = fetch(imgURL, {
-        mode: 'cors'
-      });
-    
-    newImg
-    .then(response => response.json())
-    .then(response => {
-        if (response.meta.status === 200) {
-            const gifUrl = response.data.images.downsized.url;
-            img.src = gifUrl;
-        }
-    });
+function searchObject(object, value) {
+    const properties = Object.values(object);
     
 }
+
+let myObj = {
+    name: 'Dani',
+    name: 28
+}
+
+console.log(searchObject(myObj, 28));
