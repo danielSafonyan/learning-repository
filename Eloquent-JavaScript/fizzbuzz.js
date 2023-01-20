2892f306aadcbe671cf80f0132b918766ade8363
danielSafonyan <danielsafonyan@gmail.com>
2023-01-20 10:48:17 +0000

    Restructuring learning repo

--- Eloquent-JavaScript/fizzbuzz.js
+++
@@ -1,8 +0,0 @@
-for (let i = 1; i < 101; i++) {
-    let outputMessage = "";
-    if (i % 3 == 0) outputMessage += "Fizz";
-    if (i % 5 == 0) outputMessage += "Buzz";
-    if (outputMessage == "") outputMessage  = i;
-
-    console.log(outputMessage);
-}
